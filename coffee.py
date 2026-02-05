0a204eb5b156c98638e4f243613548df37e70116
17Hamz <iah6@kent.ac.uk>
2026-02-05 10:54:50 +0000

    coffee change

--- coffee.py
+++ coffee.py
@@ -1,1 +1,19 @@
-#Your code goes here
+coins = (50, 20, 10,5)
+def main ():
+    amount_due = 75 
+    while amount_due > 0:
+     coin = int(input("Please insert a coin:"))
+
+     if coin in coins:
+        amount_due -=coin 
+        print(amount_due)
+
+    after_payment(abs(amount_due))
+
+def after_payment(moneyleftover):
+   if moneyleftover > 0:
+      
+      
+   
+
+main()
